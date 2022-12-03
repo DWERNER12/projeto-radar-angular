2d246a19dd7db44a8f4ac78f8f5722d7bf040f1e
DWERNER12 <davidwerner1995@gmail.com>
2022-12-03 20:05:10 -0300

    descomentendo-logadoService

--- src/app/paginas/cadastro-clientes/cadastro-clientes.component.ts
+++ src/app/paginas/cadastro-clientes/cadastro-clientes.component.ts
@@ -21,7 +21,7 @@ export class CadastroClientesComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    //if(this.logadoService.redirecionaLoginNaoLogado()) return
+    if(this.logadoService.redirecionaLoginNaoLogado()) return
    let id:Number = this.routerParams.snapshot.params['id']
     if(id){
       
