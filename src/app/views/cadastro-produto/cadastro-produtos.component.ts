7de27dc3dbfc407baf2ac2220479baa0a7aff7e7
jazielmatos <jazielmatos82@gmail.com>
2023-01-19 01:09:36 -0300

    Adicionando melhorias na transicao de abas

--- src/app/views/cadastro-produto/cadastro-produtos.component.ts
+++ src/app/views/cadastro-produto/cadastro-produtos.component.ts
@@ -37,12 +37,12 @@ export class CadastroProdutosComponent implements OnInit {
     console.log(this.produto)
   }
 
-  salvar(){
+  async salvar(){
     if(this.produto && this.produto.id > 0){
-      this.produtoServico.editarProduto(this.produto)
+      await this.produtoServico.editarProduto(this.produto)
     }
     else{
-      this.produtoServico.criarProduto({
+      await this.produtoServico.criarProduto({
         id: 0, 
         nome: this.produto?.nome,
         descricao: this.produto?.descricao,
